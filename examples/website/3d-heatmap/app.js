a3534b30a7bc2e3138fb2912370eb419ef6cfccb
ugur22 <ugurertas9@gmail.com>
2019-04-16 17:41:36 +0200

    added HexagonLayer

--- examples/website/3d-heatmap/app.js
+++ examples/website/3d-heatmap/app.js
@@ -2,32 +2,56 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {StaticMap} from 'react-map-gl';
-import DeckGL, {HexagonLayer} from 'deck.gl';
+import {PhongMaterial} from '@luma.gl/core';
+import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
+import {HexagonLayer} from '@deck.gl/aggregation-layers';
+import DeckGL from '@deck.gl/react';
+import {datapoints} from './data';
+import { mapStyle } from "./mapStyle.js"
+import { scaleLinear } from "d3-scale"
+
 
 // Set your mapbox token here
-const MAPBOX_TOKEN = 'pk.eyJ1IjoidWd1cjIyIiwiYSI6ImNqc2N6azM5bTAxc240M3J4MXZ1bDVyNHMifQ.rI_KbRwW8MShCcPNLsB6zA' // eslint-disable-line
+const MAPBOX_TOKEN = 'pk.eyJ1IjoidWd1cjIyIiwiYSI6ImNqc2N6azM5bTAxc240M3J4MXZ1bDVyNHMifQ.rI_KbRwW8MShCcPNLsB6zA'; // eslint-disable-line
 
 // Source data CSV
 const DATA_URL =
-  'https://raw.githubusercontent.com/Ugur22/TamTamAssigment/master/data.csv'; // eslint-disable-line
+  'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json';
+
+const ambientLight = new AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0
+});
+
+const pointLight1 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-0.144528, 49.739968, 80000]
+});
+
+const pointLight2 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-3.807751, 54.104682, 8000]
+});
+
+const lightingEffect = new LightingEffect({ambientLight, pointLight1, pointLight2});
+
+const material = new PhongMaterial({
+  ambient: 0.64,
+  diffuse: 0.6,
+  shininess: 32,
+  specularColor: [51, 51, 51]
+});
  
 export const INITIAL_VIEW_STATE = {
-  longitude:  4.728581,
-  latitude: 52.398437	,
-  zoom: 6.6,
+  longitude: 4.5881,
+  latitude: 52.4965,
+  zoom: 11,
   minZoom: 5,
   maxZoom: 15,
   pitch: 40.5,
   bearing: -27.396674584323023
-};
-
-const LIGHT_SETTINGS = {
-  lightsPosition: [-0.144528, 49.739968, 8000, -3.807751, 54.104682, 8000],
-  ambientRatio: 0.4,
-  diffuseRatio: 0.6,
-  specularRatio: 0.2,
-  lightsStrength: [0.8, 0.0, 0.8, 0.0],
-  numberOfLights: 2
 };
 
 const colorRange = [
@@ -39,7 +63,7 @@ const colorRange = [
   [209, 55, 78]
 ];
 
-const elevationScale = {min: 1, max: 50};
+let elevationScale = {min: 1, max: 50};
 
 /* eslint-disable react/no-deprecated */
 export class App extends Component {
@@ -99,24 +123,36 @@ export class App extends Component {
   }
 
   _renderLayers() {
-    const {data, radius = 1000, upperPercentile = 100, coverage = 1} = this.props;
+    const {data = datapoints, radius = 1000, upperPercentile = 100, zoom,coverage = 0.3} = this.props;
 
+    const cellSizeScale = scaleLinear([16, 6], [40, 300])
+    const cellSize = cellSizeScale(zoom)
+
+
+    const elevation = (value, zoom) => {
+      const elevationMaxScale = scaleLinear([16, 6], [400, 3000])
+      const elevationScale = scaleLinear(range, [0, elevationMaxScale(zoom)])
+
+      return elevationScale(value)
+    }
     return [
       new HexagonLayer({
         id: 'heatmap',
         colorRange,
         coverage,
         data,
-        elevationRange: [0, 1000],
-        elevationScale: this.state.elevationScale,
+        getElevation: d => elevation(d.mean, zoom),
+        updateTriggers: {
+          getElevation: [zoom]
+        },
         extruded: true,
-        getPosition: d => d,
-        lightSettings: LIGHT_SETTINGS,
+        getPosition: d => d.coordinates,
         onHover: this.props.onHover,
         opacity: 1,
         pickable: Boolean(this.props.onHover),
         radius,
-        upperPercentile
+        upperPercentile,
+        material
       })
     ];
   }
@@ -127,17 +163,13 @@ export class App extends Component {
     return (
       <DeckGL
         layers={this._renderLayers()}
+        effects={[lightingEffect]}
         initialViewState={INITIAL_VIEW_STATE}
         viewState={viewState}
         controller={controller}
       >
         {baseMap && (
-          <StaticMap
-            reuseMaps
-            mapStyle="mapbox://styles/mapbox/dark-v9"
-            preventStyleDiffing={true}
-            mapboxApiAccessToken={MAPBOX_TOKEN}
-          />
+           <StaticMap mapStyle={mapStyle} />
         )}
       </DeckGL>
     );
@@ -146,11 +178,4 @@ export class App extends Component {
 
 export function renderToDOM(container) {
   render(<App />, container);
-
-  require('d3-request').csv(DATA_URL, (error, response) => {
-    if (!error) {
-      const data = response.map(d => [Number(d.lng), Number(d.lat)]);
-      render(<App data={data} />, container);
-    }
-  });
 }
---
+++ examples/website/3d-heatmap/data.js
@@ -0,0 +1,77 @@
+export const datapoints = [
+    {
+        coordinates: [4.5881, 52.4965],
+        mean: 3,
+        max: 5
+      },
+      {
+        coordinates: [4.5962, 52.4877],
+        mean: 3,
+        max: 4
+      },
+      {
+        coordinates: [4.6101, 52.489],
+        mean: 6,
+        max: 8
+      },
+      {
+        coordinates: [4.5823, 52.4803],
+        mean: 3,
+        max: 4.5
+      },
+      {
+        coordinates: [4.5972, 52.4952],
+        mean: 8,
+        max: 10
+      },
+      {
+        coordinates: [4.604, 52.4893],
+        mean: 4,
+        max: 5.5
+      },
+      {
+        coordinates: [4.5953, 52.4962],
+        mean: 9,
+        max: 9.5
+      },
+      {
+        coordinates: [4.5906, 52.4884],
+        mean: 4,
+        max: 4.5
+      },
+      {
+        coordinates: [4.6046, 52.4852],
+        mean: 4,
+        max: 5
+      },
+      {
+        coordinates: [4.615, 52.4871],
+        mean: 5,
+        max: 6
+      },
+      {
+        coordinates: [4.616, 52.4954],
+        mean: 6,
+        max: 7.5
+      },
+      {
+        coordinates: [4.626, 52.4844],
+        mean: 4,
+        max: 6
+      },
+      {
+        coordinates: [4.6052, 52.4946],
+        mean: 8,
+        max: 8.5
+      },
+      {
+        coordinates: [4.584, 52.489],
+        mean: 9,
+        max: 11
+      },
+      {
+        coordinates: [4.5985, 52.4952],
+        mean: 5,
+        max: 7
+      }
+];
